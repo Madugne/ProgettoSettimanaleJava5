ac03f0152b049c92ca3588f888346e7f585fa7b3
Fabio Yu <fabioyu7665@gmail.com>
2024-04-12 11:40:10 +0200

    Initial commit

---
+++ src/main/java/chunyin/ProgettoSettimanale5/ProgettoSettimanale5Application.java
@@ -0,0 +1,13 @@
+package chunyin.ProgettoSettimanale5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoSettimanale5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoSettimanale5Application.class, args);
+	}
+
+}
---
+++ src/test/java/chunyin/ProgettoSettimanale5/ProgettoSettimanale5ApplicationTests.java
@@ -0,0 +1,13 @@
+package chunyin.ProgettoSettimanale5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoSettimanale5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
